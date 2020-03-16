93bb47dcdc3091cc9d1aebca3deb68230ad83246
GOUPY Gaspard <daglas@mxp.localdomain>
2020-03-15 20:50:31 -0600

    Working on spikes

---
+++ Assets/damage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+	void OnTriggerStay2D (Collider2D thing)
+	{
+		if (thing.gameObject.tag == "Player") 
+		{
+			if (thing.gameObject.GetComponent<player>().isAlive == true) 
+			{
+				thing.gameObject.GetComponent<player>().life -= 1000;
+			}
+		}
+	}
+}
---
+++ Assets/spikes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikes : MonoBehaviour
+{
+	public Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = new Vector3(
+			target.position.x, 
+			target.position.y-16, //TEMP (need to find a way to get world unit of screen height size) 
+			transform.position.z);
+    }
+}
